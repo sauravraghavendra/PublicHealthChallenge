d876e430e31ab877ab5816ea2b95f9c3e569996c
hsmath <hsmath@valleychristian.net>
2018-11-02 14:28:44 -0700

    update graph

--- R/percent_change_bar_graph.R
+++ R/percent_change_bar_graph.R
@@ -1,13 +1,15 @@
 install.packages("dplyr")
 library(dplyr)
 
-newdata <- opioid_deaths_15_16 %>%
+opioid_deaths_15_16 %>%
   group_by(State) %>%
   mutate(Diff = Crude.Rate - lag(Crude.Rate))
 
 print(newdata)
 
 counts <- newdata$Diff
+xaxis <- newdata$State
 
-barplot(counts, names.arg = newdata$State, main = "Change in Opioid Deaths 2015-2016", xlab = "State", ylab = "Difference in Crude Rate", col = "magenta")
+
+barplot(counts, names.arg = xaxis, main = "Change in Opioid Deaths 2015-2016", xlab = "State", ylab = "Difference in Crude Rate", col = "magenta", las=3)
 
