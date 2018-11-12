430ae789d43b2487f6b5562907909b5d39e6b4e0
hsmath <hsmath@valleychristian.net>
2018-11-12 14:34:29 -0800

    things

--- R/percent_change_bar_graph.R
+++ R/percent_change_bar_graph.R
@@ -9,10 +9,18 @@ updateddata <- na.omit(opioid_deaths_15_16)
 
 print(updateddata)
 
+u1 <-mean(updateddata$Diff)
+print(u1)
+
+sd <- sd(updateddata$Diff)
+print(sd)
+
+
+u2 <- mean(updateddata$Diff, na.rm = TRUE, include(c(State, "Alabama")))
 
 counts <- updateddata$Diff
 xaxis <- updateddata$State
 
-barplot(counts,  ylim = c(-5, 20), names.arg = xaxis, pos = -.2, main = "Change in Opioid Deaths 2015-2016",
+barplot(counts,  ylim = c(-5, 20), names.arg = xaxis, pos = -.2, main = "Change in Opioid Deaths 2015-2016", xlab = "State",
         ylab = "Difference in Crude Rate", col = "red", las=3)
 
